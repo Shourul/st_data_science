6d33a22d3a85b05245c0ef6f8520361fa389c19b
Shourul <shourul@mail.ru>
2021-09-27 00:56:29 +0300

    v2

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ number = np.random.randint(1, 101) # загадываем число
 count = 0
 
 while True:
-    count+=1
+    count += 1
     predict_number = int(input("Угадай число от 1 до 100: "))
     
     if predict_number > number:
--- game_v2.py
+++ game_v2.py
@@ -14,13 +14,22 @@ def random_predict(number: int = 1) -> int:
     Returns:
         int: Число попыток
     """
-    count = 0
-
+    count = 0          # Счетчик попыток отгадывания
+    min_number = 1     # Начало интервала отгадывания, первоначально 1
+    max_number = 100   # Конец интервала отгадывания, первоначально 100
+    
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101)  # предполагаемое число
+        predict_number = np.random.randint(min_number, max_number + 1)  
+            # предполагаемое число, выбранное случайно
+
         if number == predict_number:
-            break  # выход из цикла если угадали
+            break                          # выход из цикла если угадали
+        elif predict_number > number:
+            max_number = predict_number    # если не угадали,
+        else:                              # уменьшаем интервал отгадывания
+            min_number = predict_number
+        
     return count
 
 
